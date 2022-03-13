9f0900fc0610d646722daa832eec5bf877a87876
Rónán Carrigan <rcarriga@tcd.ie>
2022-03-13 13:51:12 +0000

    feat: init commit

---
+++ lua/neotest-vim-test/init.lua
@@ -0,0 +1,120 @@
+local async = require("plenary.async")
+local parse = require("neotest-vim-test.parse")
+local lib = require("neotest.lib")
+
+---@type NeotestAdapter
+local VimTestNeotestAdapter = { name = "neotest-vim-test" }
+
+local get_root = function()
+  return vim.g["test#project_root"]
+end
+
+VimTestNeotestAdapter.root = get_root
+
+function VimTestNeotestAdapter.is_test_file(file_path)
+  return async.fn["test#test_file"](file_path) == 1
+end
+
+local function in_project_root(func)
+  local cwd = async.fn.getcwd()
+  local root = get_root()
+  if root then
+    vim.cmd("cd " .. root)
+  end
+  local res = func()
+  if root then
+    vim.cmd("cd " .. cwd)
+  end
+  return res
+end
+
+local get_runner = function(file)
+  return in_project_root(function()
+    return async.fn["test#determine_runner"](file)
+  end)
+end
+
+---@param test NeotestPosition
+local function build_cmd(test)
+  return in_project_root(function()
+    local runner = get_runner(test.path)
+    local executable = async.fn["test#base#executable"](runner)
+
+    local base_args = async.fn["test#base#build_position"](
+      runner,
+      "nearest",
+      { file = test.path, line = test.range[1] + 1, col = test.range[2] + 1 }
+    )
+    local args = async.fn["test#base#options"](runner, base_args)
+    args = async.fn["test#base#build_args"](runner, args, "ultest")
+
+    local cmd = vim.list_extend(vim.split(executable, " ", { trimempty = true }), args)
+
+    cmd = lib.func_util.filter_list(function(val)
+      return val ~= ""
+    end, cmd)
+    async.util.scheduler()
+    if vim.g["test#transformation"] then
+      cmd = vim.g["test#custom_transformations"][vim.g["test#transformation"]](cmd)
+    end
+    return cmd
+  end)
+end
+
+local function get_patterns(file_name)
+  local runner = get_runner(file_name)
+  if type(runner) == "number" then
+    return {}
+  end
+  local file_type = vim.split(runner, "#", { plain = true })[1]
+  local _, patterns = pcall(function()
+    return vim.g["test#" .. runner .. "#patterns"] or vim.g["test#" .. file_type .. "#patterns"]
+  end)
+  return patterns
+end
+
+---@async
+---@return Tree | nil
+function VimTestNeotestAdapter.discover_positions(path)
+  local patterns = get_patterns(path)
+  if not patterns then
+    return
+  end
+  return parse(path, patterns)
+end
+
+---@async
+---@param args NeotestRunArgs
+---@return NeotestRunSpec
+function VimTestNeotestAdapter.build_spec(args)
+  local position = args.tree:data()
+  if position.type ~= "test" then
+    return
+  end
+  local command = build_cmd(position)
+  return {
+    command = command,
+    context = {
+      pos_id = position.id,
+    },
+  }
+end
+
+---@async
+---@param spec NeotestRunSpec
+---@param result NeotestStrategyResult
+---@return table<string, NeotestResult>
+function VimTestNeotestAdapter.results(spec, result)
+  local pos_id = spec.context.pos_id
+  return { [pos_id] = {
+    status = result.code == 0 and "passed" or "failed",
+  } }
+end
+
+setmetatable(VimTestNeotestAdapter, {
+  __call = function(_, opts)
+    return VimTestNeotestAdapter
+  end,
+})
+
+return VimTestNeotestAdapter
---
+++ lua/neotest-vim-test/parse.lua
@@ -0,0 +1,88 @@
+local Tree = require("neotest.types").Tree
+local async = require("plenary.async")
+
+local function find_match(patterns, text)
+  for _, pattern in ipairs(patterns) do
+    local matches = async.fn.matchlist(text, pattern)
+    if matches[1] then
+      return matches[2] or matches[1]
+    end
+  end
+end
+
+local function parse_buf_positions(file_path, patterns, lines)
+  local line_no = 1
+  local namespace_indent = -1
+  async.util.scheduler()
+  local function parse_position_tree(cur_namesspaces)
+    local namespace_positions = {}
+    while line_no <= #lines do
+      local line = lines[line_no]
+      local test_name = find_match(patterns.test, line)
+      local namespace_name = find_match(patterns.namespace, line)
+      line_no = line_no + 1
+      if test_name or namespace_name then
+        local pos_type = test_name and "test" or "namespace"
+        local name = test_name or namespace_name
+        local pos_id = (table.concat(cur_namesspaces, "::") .. "::" .. name)
+        local line_indent = find_match({ [[\v^(\s*)]] }, line)
+        -- Gone out of the current namespace, drop found position and exit
+        if #line_indent <= namespace_indent then
+          line_no = line_no - 1
+          return namespace_positions
+        end
+        local pos = {
+          id = pos_id,
+          type = pos_type,
+          name = name,
+          path = file_path,
+          range = { line_no - 2, 0 }, -- Will update range later
+        }
+        if pos_type == "namespace" then
+          local next_namespaces = vim.list_extend(vim.list_extend({}, cur_namesspaces), { name })
+          local prev_indent = namespace_indent
+          namespace_indent = #line_indent
+          local children = parse_position_tree(next_namespaces)
+          namespace_indent = prev_indent
+          if #children > 0 then
+            table.insert(namespace_positions, vim.list_extend({ pos }, children))
+          end
+        else
+          table.insert(namespace_positions, pos)
+        end
+      end
+    end
+    return namespace_positions
+  end
+  local parsed = parse_position_tree({ file_path })
+  table.insert(parsed, 1, {
+    type = "file",
+    path = file_path,
+    name = async.fn.fnamemodify(file_path, ":t"),
+    id = file_path,
+    range = { 0, 0, #lines, 0 },
+  })
+  local tree = Tree.from_list(parsed, function(pos)
+    return pos.id
+  end)
+  local pos_end = #lines - 1
+  local reversed = {}
+  for _, pos in tree:iter() do
+    table.insert(reversed, 1, pos)
+  end
+  for _, pos in ipairs(reversed) do
+    pos.range[3] = pos_end
+    pos.range[4] = vim.str_utfindex(lines[pos_end + 1])
+    pos_end = pos.range[1] - 1
+  end
+  return tree
+end
+
+return function(file_path, patterns)
+  local lines = require("neotest.lib").files.read_lines(file_path)
+  if #lines == 0 then
+    return
+  end
+  local result = parse_buf_positions(file_path, patterns, lines)
+  return result
+end
